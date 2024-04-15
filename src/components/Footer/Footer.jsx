4969952608e0e904e8b8e990e96670bbf78a77e2
Anna Zubchenko <annazubchenko282@gmail.com>
2024-04-15 20:14:27 +0300

    changed vite.config

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import sprite from '../../../public/sprite.svg';
+import sprite from '../../assets/sprite.svg';
 
 const Footer = () => {
   return (
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/test-feedback-form',
   plugins: [react()],
+  server: {
+    // Настройки CORS
+    cors: true,
+    // Дополнительные заголовки CORS, если необходимо
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    },
+  },
 })
