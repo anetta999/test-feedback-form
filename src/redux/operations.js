dea92ef6a0d958375250b87293db6a90bda99ac0
Anna Zubchenko <annazubchenko282@gmail.com>
2024-04-18 14:42:45 +0300

    changed api logic

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
-axios.defaults.baseURL = 'https://661d4a6298427bbbef016077.mockapi.io';
+axios.defaults.baseURL = 'https://test-feedback-form-node-js.onrender.com';
 
 export const fetchFormData = createAsyncThunk(
   'form/fetchFormData',
   async (formData, thunkAPI) => {
 
     try {
-      const { data } = await axios.post('/applications', {formData});
+      const { data } = await axios.post('/feedbacks', {formData});
       return data;
     } catch (error) {
       toast.error('Something went wrong, please try reloading the page', {
